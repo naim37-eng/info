e9e4c0bec8ee8bdc48a31dc0c11de4b52954efd8
naim <naimbhuiyan01884530@gmail.com>
2026-04-04 20:28:11 +0600

    update

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const faqItems = document.querySelectorAll('.faq-item');
 faqItems.forEach(item => {
     item.addEventListener('click', () => {
         faqItems.forEach(el => {
-            if (el === item) {
-               item.classList.toggle('active');
+            if (el !== item) {
+               el.classList.remove('active');
             }
+          
         });
-        
+        item.classList.toggle('active');
     });
 });
